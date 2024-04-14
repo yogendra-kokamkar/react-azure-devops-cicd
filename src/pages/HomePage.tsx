6d97ad3548ea0340330d169c44029009a73f6355
yogendra-kokamkar <yogendrak43@gmail.com>
2024-04-14 23:49:16 +0530

    Updated webpage message

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,7 +4,7 @@ import DateDisplay from '../components/DateDisplay'
 const HomePage: React.FC = () => {
     return (
         <div style={{ position: 'relative', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
-            <h1 style={{ fontSize: '4em' }}>Hello from Bit2!</h1>
+            <h1 style={{ fontSize: '4em' }}>Hello from Bit2 !!</h1>
             <DateDisplay />
         </div>
     )
